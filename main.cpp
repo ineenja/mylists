d60ae8a60c07064bacfc81d35db8cff1d2493f19
Andrey Tasmagambetov <theiznogoud@gmail.com>
2025-02-07 15:32:04 +0300

    fixed errors in basic functions

--- main.cpp
+++ main.cpp
@@ -5,19 +5,31 @@
 
 int main()
 {
+    std::list<int> test;
+    test.push_back(3);
 
-    //MyList<int> mylist(1,1);
-    MyList<int> mylist;
-    mylist.pushBack(1);
-    mylist.pushBack(2);
-    mylist.pushBack(3);
+    test.front() = 4;
 
-    std::cout << mylist.front()->value << std::endl;
-    std::cout << mylist.back()->value << std::endl;
+    std::cout << test.front()+1 << std::endl;
 
-    mylist.popFront();
+    // MyList<int> mylist;
+    //
+    // mylist.pushBack(1);
+    // mylist.pushBack(2);
+    // mylist.pushBack(3);
+    //
+    // std::cout << mylist.front()->value << std::endl;
+    // std::cout << mylist.back()->value << std::endl;
+    //
+    // mylist.popFront();
+    //
+    // std::cout << mylist.front()->value << std::endl;
+    //
+    // MyList<int> mylist2(1,1);
 
-    std::cout << mylist.front()->value << std::endl;
+    // MyList<int> mylist3{1, 2, 3, 4};
+    // std::cout << mylist3.front()->value << std::endl;
+    // std::cout << mylist3.back()->value << std::endl;
 
     std::cout << "---" << std::endl;
     return 1;
--- mylist/test/mylist_test.cpp
+++ mylist/test/mylist_test.cpp
@@ -1,14 +1,208 @@
 #include <gtest/gtest.h>
 #include <iostream>
+#include <list>
+#include <mylist/mylist.h>
 
-//TEST(ObjectValueTest, MySharedPtrTests) {
-//    int test1 = 1;
-//    MySharedPtr<int> myptr {makeShared<int>(test1)};
-//    std::shared_ptr<int> ptr {std::make_shared<int>(test1)};
+//////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////// ТЕСТЫ ДЛЯ КОНСТРУКТОРОВ ///////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
 
-//    ASSERT_EQ(*myptr, *ptr);
-//}
+/// тест конструктора по умолчанию
+TEST(MyListTests, Constructor1IsEmpty) {
 
+    MyList<int> myList;
+    std::list<int> testList;
+
+    ASSERT_EQ(myList.empty(), testList.empty());
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+/////// ТЕСТЫ ПЕРЕДАЧИ ПОСЛЕДНЕГО ЭЛЕМЕНТА СПИСКА И ДОБАВЛЕНИЯ ЭЛЕМЕНТА В КОНЕЦ СПИСКА ///////
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+/// Тест на количество элементов списка после добавления нескольких элементов в его конец
+TEST(MyListTests, pushBackCheckSize) {
+
+    MyList<int> myList;
+    std::list<int> testList;
+
+    myList.pushBack(1);
+    testList.push_back(1);
+
+    ASSERT_EQ(myList.getSize(), testList.size());
+
+    myList.pushBack(1);
+    testList.push_back(1);
+
+    ASSERT_EQ(myList.getSize(), testList.size());
+}
+
+/// Тест возможности передачи ссылки на последний элемент списка посредством проверки его значения
+TEST(MyListTests, backCheck) {
+
+    MyList<int> myList;
+    std::list<int> testList;
+
+    myList.pushBack(1);
+    testList.push_back(1);
+
+    ASSERT_EQ(myList.back(), testList.back());
+
+    myList.pushBack(2);
+    testList.push_back(2);
+
+    ASSERT_EQ(myList.back(), testList.back());
+}
+
+/// Тест возможности изменения последнего элемента списка по передаваемой ссылке
+TEST(MyListTests, backCheckChangingValueCheck) {
+
+    MyList<int> myList;
+    std::list<int> testList;
+
+    myList.pushBack(1);
+    testList.push_back(1);
+
+    myList.back() += 1;
+    testList.back() += 1;
+
+    ASSERT_EQ(myList.back(), testList.back());
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+////// ТЕСТЫ ВОЗВРАЩЕНИЯ ПЕРВОГО ЭЛЕМЕНТА СПИСКА И ДОБАВЛЕНИЯ ЭЛЕМЕНТА В НАЧАЛО СПИСКА //////
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+/// Проверка размера списка после добавления элементов в начало
+TEST(MyListTests, pushFrontCheckSize) {
+
+    MyList<int> myList;
+    std::list<int> testList;
+
+    myList.pushFront(1);
+    testList.push_front(1);
+
+    ASSERT_EQ(myList.getSize(), testList.size());
+
+    myList.pushFront(1);
+    testList.push_front(1);
+
+    ASSERT_EQ(myList.getSize(), testList.size());
+}
+
+/// Тест возможности передачи ссылки на первый элемент списка посредством проверки его значения
+TEST(MyListTests, frontCheck) {
+
+    MyList<int> myList;
+    std::list<int> testList;
+
+    myList.pushBack(1);
+    testList.push_back(1);
+
+    ASSERT_EQ(myList.front(), testList.front());
+
+    myList.pushBack(2);
+    testList.push_back(2);
+
+    ASSERT_EQ(myList.front(), testList.front());
+}
+
+/// Тест возможности изменения значения первого элемента списка по передаваемой ссылке на него
+TEST(MyListTests, frontCheckChangingValueCheck) {
+
+    MyList<int> myList;
+    std::list<int> testList;
+
+    myList.pushBack(1);
+    testList.push_back(1);
+
+    myList.back() += 1;
+    testList.back() += 1;
+
+    ASSERT_EQ(myList.front(), testList.front());
+}
+
+////////////////////////////////////////////////////////////
+/// ТЕСТЫ УДАЛЕНИЯ ПЕРВОГО И ПОСЛЕДНЕГО ЭЛЕМЕНТОВ СПИСКА ///
+////////////////////////////////////////////////////////////
+
+/// Проверка количества элементов после удаления последних элементов списка
+TEST(MyListTests, popBackCheckSize) {
+
+    MyList<int> myList;
+    std::list<int> testList;
+
+    myList.pushBack(1);
+    testList.push_back(1);
+
+    myList.pushBack(1);
+    testList.push_back(1);
+
+    myList.popBack();
+    testList.pop_back();
+
+    ASSERT_EQ(myList.getSize(), testList.size());
+
+    myList.popBack();
+    testList.pop_back();
+
+    ASSERT_EQ(myList.getSize(), testList.size());
+}
+
+/// Проверка количества элементов после удаления первых элементов списка
+TEST(MyListTests, popFrontCheckSize) {
+
+    MyList<int> myList;
+    std::list<int> testList;
+
+    myList.pushBack(1);
+    testList.push_back(1);
+
+    myList.pushBack(1);
+    testList.push_back(1);
+
+    myList.popFront();
+    testList.pop_front();
+
+    ASSERT_EQ(myList.getSize(), testList.size());
+
+    myList.popFront();
+    testList.pop_front();
+
+    ASSERT_EQ(myList.getSize(), testList.size());
+}
+
+/// проверка функции очищения списка
+TEST(MyListTests, clearListCheck) {
+
+    MyList<int> myList;
+    std::list<int> testList;
+
+    myList.pushBack(1);
+    testList.push_back(1);
+
+    myList.pushBack(1);
+    testList.push_back(1);
+
+    myList.clearList();
+    testList.clear();
+
+    ASSERT_EQ(myList.getSize(), testList.size());
+}
+
+// TEST(MyListTests, myIteratorTest) {
+//
+//     MyList<int> myList;
+//
+//     myList.pushBack(1);
+//     myList.pushBack(2);
+//     myList.pushBack(3);
+//
+//     int test = 1;
+//     for (auto iter : myList) {
+//         std::cout << iter << std::endl;
+//     }
+// }
 
 
 
